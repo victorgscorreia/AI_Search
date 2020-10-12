8edf83cd40e3987dc14d111d7251976780b5d201
LipArcanjo <luanicaroarcanjo@gmail.com>
2020-10-12 11:03:25 -0300

    implemented A_Star + Best-FS + DFS

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def read_maze():
 
     return maze_string, size
 
+
 def creat_matrix_np(maze_string, size):
     
     maze_matrix = np.zeros([size[0],size[1]])
@@ -56,7 +57,6 @@ def creat_matrix_np(maze_string, size):
 
     return maze_matrix,ini,end
 
-
 def next_steps(element,size,visitado,maze_matrix):
     moves = [[-1,0],[1,0],[0,-1],[0,1]]
     
@@ -80,21 +80,87 @@ def next_steps(element,size,visitado,maze_matrix):
     return nexts
 
 
-def largura(maze_matrix,ini,end, size,display):
+def print_caminho(i,j, anterior_i, anterior_j, display,ini,end):
+    i = int(i)
+    j = int(j)
+    if anterior_i[i][j] != -1:
+        print_caminho( anterior_i[i][j], anterior_j[i][j], anterior_i,anterior_j,display,ini,end ) 
+        
+    if (i != ini[0] or j != ini[1]) and (i != end[0] or j != end[1]):
+        display.drawStep(i,j,COLOR_RIGHT_PATH)
+    time.sleep(TIME_WAIT)  
+
+class Cell:
+
+    def __init__(self, i, j, depth, heuristica):
+        self.i = i
+        self.j = j
+        self.depth = depth
+        self.h = heuristica
+    
+    #as funcoes abaixo deixam a classe comparavel
+    def __lt__(self,other):
+        return self.h + self.depth < other.h + other.depth
+
+    def __eq__(self,other):
+        return self.h + self.depth == other.h + other.depth
+
+    def __ne__(self,other):
+        return self.h + self.depth != other.h + other.depth
+
+    def __le__(self,other):
+        return self.h + self.depth <= other.h + other.depth
+
+    def __gt__(self,other):
+        return self.h + self.depth > other.h + other.depth
+
+    def __ge__(self,other):
+        return self.h + self.depth >= other.h + other.depth
+
+def heuristica_manhattan(i,j, i_target, j_target):
+    return abs(i - i_target) + abs(j - j_target)
+
+def heu_equal(i,j,i_target,j_target):
+    heu = 0
+    if i != i_target:
+        heu += 1
+    if j != j_target:
+        heu += 1
+    return heu
+
+def heuristica_euclidiana(i,j, i_target, j_target):
+    a = i - i_target
+    b = j - j_target
+    return np.sqrt(a*a + b*b)
+
+def A_Star(maze_matrix,ini,end, size, heuristica,display):
+
     visitado = np.zeros((size[0],size[1]))
     anterior_i = -1*np.ones((size[0],size[1]))
     anterior_j = -1*np.ones((size[0],size[1]))
-    q = queue.Queue()
 
-    q.put(ini)
+    q = queue.PriorityQueue()
+
+    heu = heuristica(ini[0],ini[1],end[0],end[1])
+    ini_cell = Cell(ini[0],ini[1],0,heu)
+    q.put(ini_cell)
+    
+    count_visited = 0
+    count_in_queue = 0
 
     while not q.empty():
-        ele = q.get()
+        ele_cell = q.get()
+        ele = np.zeros((2,)).astype(int)
+
+        ele[0] = ele_cell.i
+        ele[1] = ele_cell.j
+
         visitado[ele[0],ele[1]] = 2
         
+        count_visited += 1
 
         if ele[0] == end[0] and ele[1] == end[1]:
-            return anterior_i,anterior_j
+            return anterior_i,anterior_j, count_visited, count_in_queue
         
         nexts = next_steps(ele,size,visitado,maze_matrix)
 
@@ -107,26 +173,156 @@ def largura(maze_matrix,ini,end, size,display):
             anterior_j[n[0]][n[1]] = ele[1]
 
             visitado[n[0]][n[1]] = 1
-
-            q.put(n)
+            
+            count_in_queue += 1
+            n_heu = heuristica(n[0],n[1],end[0],end[1])
+            n_cell = Cell(n[0],n[1],ele_cell.depth+1, n_heu)
+            q.put(n_cell)
         
         #nao pintar o ultimo nem o primeiro
         if ele[0] != ini[0] or ele[1] != ini[1]:
             display.drawStep(ele[0],ele[1],COLOR_VISITED)
         time.sleep(TIME_WAIT)
             
-    return None,None
+    return None,None, None, None
 
-def print_caminho(i,j, anterior_i, anterior_j, display,ini,end):
-    i = int(i)
-    j = int(j)
-    if anterior_i[i][j] != -1:
-        print_caminho( anterior_i[i][j], anterior_j[i][j], anterior_i,anterior_j,display,ini,end ) 
+def largura(maze_matrix,ini,end, size,display):
+
+    visitado = np.zeros((size[0],size[1]))
+    anterior_i = -1*np.ones((size[0],size[1]))
+    anterior_j = -1*np.ones((size[0],size[1]))
+    q = queue.Queue()
+
+    q.put(ini)
+    
+    count_visited = 0
+    count_in_queue = 0
+
+    while not q.empty():
+        ele = q.get()
+        visitado[ele[0],ele[1]] = 2
         
-    if (i != ini[0] or j != ini[1]) and (i != end[0] or j != end[1]):
-        display.drawStep(i,j,COLOR_RIGHT_PATH)
-    time.sleep(TIME_WAIT)   
+        count_visited += 1
 
+        if ele[0] == end[0] and ele[1] == end[1]:
+            return anterior_i,anterior_j, count_visited, count_in_queue
+        
+        nexts = next_steps(ele,size,visitado,maze_matrix)
+
+        for n in nexts:
+            if n[0] != end[0] or n[1] != end[1]:
+                display.drawStep(n[0],n[1],COLOR_IN_QUEUE)
+
+            anterior_i[n[0]][n[1]] = ele[0]
+
+            anterior_j[n[0]][n[1]] = ele[1]
+
+            visitado[n[0]][n[1]] = 1
+            
+            count_in_queue += 1
+
+            q.put(n)
+        
+        #nao pintar o ultimo nem o primeiro
+        if ele[0] != ini[0] or ele[1] != ini[1]:
+            display.drawStep(ele[0],ele[1],COLOR_VISITED)
+            time.sleep(TIME_WAIT)
+            
+    return None,None, None, None
+
+def profundidade(maze_matrix,ini,end, size,display):
+
+    visitado = np.zeros((size[0],size[1]))
+    anterior_i = -1*np.ones((size[0],size[1]))
+    anterior_j = -1*np.ones((size[0],size[1]))
+    q = queue.LifoQueue()
+
+    q.put(ini)
+    
+    count_visited = 0
+    count_in_queue = 0
+
+    while not q.empty():
+        ele = q.get()
+        visitado[ele[0],ele[1]] = 2
+        
+        count_visited += 1
+
+        if ele[0] == end[0] and ele[1] == end[1]:
+            return anterior_i,anterior_j, count_visited, count_in_queue
+        
+        nexts = next_steps(ele,size,visitado,maze_matrix)
+
+        for n in nexts:
+            if n[0] != end[0] or n[1] != end[1]:
+                display.drawStep(n[0],n[1],COLOR_IN_QUEUE)
+
+            anterior_i[n[0]][n[1]] = ele[0]
+
+            anterior_j[n[0]][n[1]] = ele[1]
+
+            visitado[n[0]][n[1]] = 1
+            
+            count_in_queue += 1
+
+            q.put(n)
+        
+        #nao pintar o ultimo nem o primeiro
+        if ele[0] != ini[0] or ele[1] != ini[1]:
+            display.drawStep(ele[0],ele[1],COLOR_VISITED)
+            time.sleep(TIME_WAIT)
+            
+    return None,None, None, None
+
+def best_first_search(maze_matrix,ini,end, size,display):
+    visitado = np.zeros((size[0],size[1]))
+    anterior_i = -1*np.ones((size[0],size[1]))
+    anterior_j = -1*np.ones((size[0],size[1]))
+
+    q = queue.PriorityQueue()
+
+    ini_cell = Cell(ini[0],ini[1],0,0)
+    q.put(ini_cell)
+    
+    count_visited = 0
+    count_in_queue = 0
+
+    while not q.empty():
+        ele_cell = q.get()
+        ele = np.zeros((2,)).astype(int)
+
+        ele[0] = ele_cell.i
+        ele[1] = ele_cell.j
+
+        visitado[ele[0],ele[1]] = 2
+        
+        count_visited += 1
+
+        if ele[0] == end[0] and ele[1] == end[1]:
+            return anterior_i,anterior_j, count_visited, count_in_queue
+        
+        nexts = next_steps(ele,size,visitado,maze_matrix)
+
+        for n in nexts:
+            if n[0] != end[0] or n[1] != end[1]:
+                display.drawStep(n[0],n[1],COLOR_IN_QUEUE)
+
+            anterior_i[n[0]][n[1]] = ele[0]
+
+            anterior_j[n[0]][n[1]] = ele[1]
+
+            visitado[n[0]][n[1]] = 1
+            
+            count_in_queue += 1
+            n_cell = Cell(n[0],n[1],ele_cell.depth+1, 0)
+            q.put(n_cell)
+        
+        #nao pintar o ultimo nem o primeiro
+        if ele[0] != ini[0] or ele[1] != ini[1]:
+            display.drawStep(ele[0],ele[1],COLOR_VISITED)
+        time.sleep(TIME_WAIT)
+            
+    return None,None, None, None
 
 def main():
 
@@ -137,7 +333,7 @@ def main():
     display = dw.DisplayMaze(800, 800, maze_string)
     
 
-    ant_i,ant_j = largura(maze_matrix,ini,end,size,display)
+    ant_i,ant_j,c_v,c_q = A_Star(maze_matrix,ini,end,size,heuristica_manhattan,display)
     
     if ant_i is not None:
         print_caminho(end[0], end[1],ant_i, ant_j,display,ini,end)
