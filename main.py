1295321cd2b48883c991a544429f570ad3e294ae
LipArcanjo <luanicaroarcanjo@gmail.com>
2020-10-12 12:30:09 -0300

    adicionado tamanho maximo da estrutura

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import numpy as np
 import draw as dw
 import time
 import queue
+import random
 
 COLOR_RIGHT_PATH = [0,255,127]
 COLOR_VISITED = [255, 203, 219]
 COLOR_IN_QUEUE = [128,128,128]
-TIME_WAIT = 0.05
+TIME_WAIT = 0.001
 
 
 def read_maze():
@@ -83,12 +84,15 @@ def next_steps(element,size,visitado,maze_matrix):
 def print_caminho(i,j, anterior_i, anterior_j, display,ini,end):
     i = int(i)
     j = int(j)
+    ret = 1
     if anterior_i[i][j] != -1:
-        print_caminho( anterior_i[i][j], anterior_j[i][j], anterior_i,anterior_j,display,ini,end ) 
+        ret += print_caminho( anterior_i[i][j], anterior_j[i][j], anterior_i,anterior_j,display,ini,end ) 
         
     if (i != ini[0] or j != ini[1]) and (i != end[0] or j != end[1]):
         display.drawStep(i,j,COLOR_RIGHT_PATH)
     time.sleep(TIME_WAIT)  
+
+    return ret
 
 class Cell:
 
@@ -148,7 +152,13 @@ def A_Star(maze_matrix,ini,end, size, heuristica,display):
     count_visited = 0
     count_in_queue = 0
 
+    max_tam_queue = 1
+
     while not q.empty():
+
+        if max_tam_queue < q.qsize():
+            max_tam_queue = q.qsize()
+
         ele_cell = q.get()
         ele = np.zeros((2,)).astype(int)
 
@@ -160,7 +170,7 @@ def A_Star(maze_matrix,ini,end, size, heuristica,display):
         count_visited += 1
 
         if ele[0] == end[0] and ele[1] == end[1]:
-            return anterior_i,anterior_j, count_visited, count_in_queue
+            return anterior_i,anterior_j, count_visited, count_in_queue, max_tam_queue
         
         nexts = next_steps(ele,size,visitado,maze_matrix)
 
@@ -184,7 +194,7 @@ def A_Star(maze_matrix,ini,end, size, heuristica,display):
             display.drawStep(ele[0],ele[1],COLOR_VISITED)
         time.sleep(TIME_WAIT)
             
-    return None,None, None, None
+    return None,None, count_visited, count_in_queue, max_tam_queue
 
 def largura(maze_matrix,ini,end, size,display):
 
@@ -198,14 +208,20 @@ def largura(maze_matrix,ini,end, size,display):
     count_visited = 0
     count_in_queue = 0
 
+    max_tam_queue = 1
+
     while not q.empty():
+
+        if max_tam_queue < q.qsize():
+            max_tam_queue = q.qsize()
+
         ele = q.get()
         visitado[ele[0],ele[1]] = 2
         
         count_visited += 1
 
         if ele[0] == end[0] and ele[1] == end[1]:
-            return anterior_i,anterior_j, count_visited, count_in_queue
+            return anterior_i,anterior_j, count_visited, count_in_queue, max_tam_queue
         
         nexts = next_steps(ele,size,visitado,maze_matrix)
 
@@ -228,7 +244,7 @@ def largura(maze_matrix,ini,end, size,display):
             display.drawStep(ele[0],ele[1],COLOR_VISITED)
             time.sleep(TIME_WAIT)
             
-    return None,None, None, None
+    return None,None, count_visited, count_in_queue, max_tam_queue
 
 def profundidade(maze_matrix,ini,end, size,display):
 
@@ -242,14 +258,20 @@ def profundidade(maze_matrix,ini,end, size,display):
     count_visited = 0
     count_in_queue = 0
 
+    max_tam_queue = 1
+
     while not q.empty():
+
+        if max_tam_queue < q.qsize():
+            max_tam_queue = q.qsize()
+            
         ele = q.get()
         visitado[ele[0],ele[1]] = 2
         
         count_visited += 1
 
         if ele[0] == end[0] and ele[1] == end[1]:
-            return anterior_i,anterior_j, count_visited, count_in_queue
+            return anterior_i,anterior_j, count_visited, count_in_queue, max_tam_queue
         
         nexts = next_steps(ele,size,visitado,maze_matrix)
 
@@ -272,7 +294,7 @@ def profundidade(maze_matrix,ini,end, size,display):
             display.drawStep(ele[0],ele[1],COLOR_VISITED)
             time.sleep(TIME_WAIT)
             
-    return None,None, None, None
+    return None,None, count_visited, count_in_queue, max_tam_queue
 
 def best_first_search(maze_matrix,ini,end, size,display):
     visitado = np.zeros((size[0],size[1]))
@@ -287,7 +309,13 @@ def best_first_search(maze_matrix,ini,end, size,display):
     count_visited = 0
     count_in_queue = 0
 
+    max_tam_queue = 1
+
     while not q.empty():
+
+        if max_tam_queue < q.qsize():
+            max_tam_queue = q.qsize()
+
         ele_cell = q.get()
         ele = np.zeros((2,)).astype(int)
 
@@ -299,7 +327,7 @@ def best_first_search(maze_matrix,ini,end, size,display):
         count_visited += 1
 
         if ele[0] == end[0] and ele[1] == end[1]:
-            return anterior_i,anterior_j, count_visited, count_in_queue
+            return anterior_i,anterior_j, count_visited, count_in_queue,max_tam_queue
         
         nexts = next_steps(ele,size,visitado,maze_matrix)
 
@@ -322,7 +350,64 @@ def best_first_search(maze_matrix,ini,end, size,display):
             display.drawStep(ele[0],ele[1],COLOR_VISITED)
         time.sleep(TIME_WAIT)
             
-    return None,None, None, None
+    return None,None, count_visited, count_in_queue, max_tam_queue
+
+def hill_climbing(maze_matrix,ini,end, size, heuristica,display):
+
+    visitado = np.zeros((size[0],size[1]))
+    anterior_i = -1*np.ones((size[0],size[1]))
+    anterior_j = -1*np.ones((size[0],size[1]))
+
+    q = queue.PriorityQueue()
+
+    heu = heuristica(ini[0],ini[1],end[0],end[1])
+    ini_cell = Cell(ini[0],ini[1],0,heu)
+    q.put(ini_cell)
+    
+    count_visited = 0
+    count_in_queue = 0
+
+    while not q.empty():
+        ele_cell = q.get()
+        ele = np.zeros((2,)).astype(int)
+
+        ele[0] = ele_cell.i
+        ele[1] = ele_cell.j
+
+        visitado[ele[0],ele[1]] = 2
+        
+        count_visited += 1
+
+        if ele[0] == end[0] and ele[1] == end[1]:
+            return anterior_i,anterior_j, count_visited, count_in_queue,1
+        
+        nexts = next_steps(ele,size,visitado,maze_matrix)
+
+        min_cell = ele_cell
+
+        random.shuffle(nexts)
+        for n in nexts:
+            
+            anterior_i[n[0]][n[1]] = ele[0]
+
+            anterior_j[n[0]][n[1]] = ele[1]
+
+            visitado[n[0]][n[1]] = 1
+            
+            count_in_queue += 1
+            n_heu = heuristica(n[0],n[1],end[0],end[1])
+            n_cell = Cell(n[0],n[1],0, n_heu)
+            if n_cell <= min_cell:
+                min_cell = n_cell
+        
+        if min_cell.i != ele[0] or min_cell.j != ele[1]:
+            q.put(min_cell)
+        #nao pintar o ultimo nem o primeiro
+        if ele[0] != ini[0] or ele[1] != ini[1]:
+            display.drawStep(ele[0],ele[1],COLOR_VISITED)
+        time.sleep(TIME_WAIT)
+            
+    return None,None, count_visited, count_in_queue, 1
 
 def main():
 
@@ -332,11 +417,16 @@ def main():
 
     display = dw.DisplayMaze(800, 800, maze_string)
     
-
-    ant_i,ant_j,c_v,c_q = A_Star(maze_matrix,ini,end,size,heuristica_manhattan,display)
     
+    ant_i,ant_j,c_v,c_q,max_tam_queue = profundidade(maze_matrix,ini,end,size,display)
+    tam_caminho = 0
     if ant_i is not None:
-        print_caminho(end[0], end[1],ant_i, ant_j,display,ini,end)
+        tam_caminho = print_caminho(end[0], end[1],ant_i, ant_j,display,ini,end)
+
+    print("numero visitados = " + str(c_v))
+    print("numero colocados na fila = " + str(c_q))
+    print("tamanho maximo da estrutura de dados = " + str(max_tam_queue))
+    print("tamanho do caminho achado = " + str(tam_caminho))
 
     while display.status():
         i = 1+1
